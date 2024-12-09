716d83c242624151fe0a6385ecb938310353d19d
Kewaii <miguel.viegas@kewaii.cloud>
2024-12-09 16:24:23 +0000

    Add Blueprints

--- scripts/env.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Ian Lucas. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-import * as dotenv from "dotenv";
-dotenv.config();
-
-export const CS2_CSGO_PATH = process.env.CS2_CSGO_PATH ?? `${process.cwd()}/scripts/workdir/decompiled`;
