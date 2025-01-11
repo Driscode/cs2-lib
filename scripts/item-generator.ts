f1ea8ca618720647808e093c38bd67b823f53e0a
Kewaii <miguelviegas1999@gmail.com>
2025-01-11 17:07:10 +0000

    Update to latest ianlucas/cs2-lib

--- scripts/item-generator.ts
+++ scripts/item-generator.ts
@@ -40,6 +40,7 @@ const ITEMS_GAME_CUSTOM_JSON_PATH = "assets/data/items-game-custom.json";
 const ITEMS_JSON_PATH = "assets/data/items.json";
 const ITEMS_TS_PATH = "src/items.ts";
 const LOCALIZATIONS_JSON_PATH = "assets/localizations/items-%s.json";
+const PARSED_ITEMS_GAME_PATH = "assets/data/parsed-items-game.json";
 
 const FORMATTED_STRING_RE = /%s(\d+)/g;
 const LANGUAGE_FILE_RE = /csgo_([^\._]+)\.txt$/;
@@ -150,10 +151,10 @@ export class ItemGenerator {
         this.parseSkins();
         this.parseMusicKits();
         this.parseStickers();
+        this.parseKeychains();
         this.parseGraffiti();
         this.parsePatches();
         this.parseAgents();
-        this.parseKeychains();
         this.parseCollectibles();
         this.parseTools();
         this.parseContainers();
@@ -772,13 +773,10 @@ export class ItemGenerator {
     private parseKeychains() {
         warning("Parsing keychains...");
         const baseId = this.createStub("keychain", "#CSGO_Tool_Keychain_Desc");
-        for (const [
-            index,
-            {name, loc_name, loc_description, item_rarity, image_inventory}
-        ] of Object.entries(this.gameItems.keychain_definitions)) {
-            if (
-                !this.hasTranslation(loc_name)
-            ) {
+        for (const [index, { name, loc_name, loc_description, item_rarity, image_inventory }] of Object.entries(
+            this.gameItems.keychain_definitions
+        )) {
+            if (!this.hasTranslation(loc_name)) {
                 continue;
             }
             const id = this.itemIdentifierManager.get(`keychain_${index}`);
@@ -790,7 +788,7 @@ export class ItemGenerator {
                 baseId,
                 def: 1355,
                 id,
-                image: this.itemManager.get(id)?.image ?? this.getImage(id, `${image_inventory}`),
+                image: this.itemManager.get(id)?.image ?? this.getImage(id, image_inventory),
                 index: Number(index),
                 rarity: this.getRarityColorHex([itemKey, item_rarity]),
                 type: CS2ItemType.Keychain
@@ -864,7 +862,7 @@ export class ItemGenerator {
                 def: Number(index),
                 free: baseitem === "1" && index !== REMOVE_KEYCHAIN_TOOL_INDEX ? true : undefined,
                 id,
-                image: this.itemManager.get(id)?.image ?? this.getImage(id, image),
+                image: this.getImage(id, image),
                 index: undefined,
                 rarity: this.getRarityColorHex(["common"]),
                 teams: undefined,
@@ -895,14 +893,14 @@ export class ItemGenerator {
                 tool
             }
         ] of Object.entries(this.gameItems.items)) {
+            const hasSupplyCrateSeries =
+                attributes?.["set supply crate series"]?.attribute_class === "supply_crate_series";
             if (
                 item_name === undefined ||
                 image_inventory === undefined ||
-                !image_inventory.includes("econ/weapon_cases") ||
+                (!image_inventory.includes("econ/weapon_cases") && !hasSupplyCrateSeries) ||
                 tool?.type === "gift" ||
-                (prefab !== "weapon_case" &&
-                    attributes?.["set supply crate series"]?.attribute_class !== "supply_crate_series" &&
-                    loot_list_name === undefined)
+                (prefab !== "weapon_case" && !hasSupplyCrateSeries && loot_list_name === undefined)
             ) {
                 continue;
             }
@@ -933,6 +931,7 @@ export class ItemGenerator {
                 }
             }
             if (contents.length > 0) {
+                const thePrefab = this.tryGetPrefab(prefab);
                 // Asserts if the container requires a key.
                 assert(
                     associated_items !== undefined ||
@@ -1134,6 +1133,9 @@ export class ItemGenerator {
             warning(`Generated '${path}'.`);
         }
 
+        writeJson(PARSED_ITEMS_GAME_PATH, this.gameItems);
+        warning(`Generated '${PARSED_ITEMS_GAME_PATH}'`);
+
         writeJson(ITEMS_GAME_JSON_PATH, this.gameItems);
         warning(`Generated '${ITEMS_GAME_JSON_PATH}'.`);
 
@@ -1243,6 +1245,10 @@ export class ItemGenerator {
 
     private getPrefab(prefab?: string) {
         return ensure(this.gameItems.prefabs[ensure(prefab)]);
+    }
+
+    private tryGetPrefab(prefab?: string) {
+        return prefab !== undefined ? this.gameItems.prefabs[prefab] : undefined;
     }
 
     private getTeams(teams?: Record<string, string>) {
